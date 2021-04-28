3ef1cbcfc69d088d2650cce8e152cc4a563e7431
Dizzysch <victorwilbert@gmail.com>
2021-04-27 21:38:32 -0300

    remove dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,26 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const isDevelopment = process.env.NODE_ENV !== 'production'
 
 module.exports = {
-    mode: 'development',
+    mode: isDevelopment ? 'development' : 'production',
+    devtool: isDevelopment ? 'eval-source-map' : 'source-map',
     entry: path.resolve(__dirname, 'src', 'index.jsx'),
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html')
+        })
+    ],
     resolve: {
         extensions: ['.js', '.jsx'],
+    }, 
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
     },
     module: {
         rules: [
@@ -16,6 +28,11 @@ module.exports = {
                 test: /\.jsx$/, 
                 exclude: /node_modules/,
                 use: 'babel-loader',
+            },
+            {
+                test: /\.scss$/, 
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
             }
         ],
     }
